8f411a1a9fa58d02739da7848462799dda2cf803
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2024-12-16 12:36:54 +0000

    Login inter

--- SPRecipeManager/Program.cs
+++ SPRecipeManager/Program.cs
@@ -7,7 +7,20 @@ namespace SPRecipeManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool loginRun = true;
+
+            while (loginRun)
+            {
+                Console.WriteLine("=========================");
+                Console.WriteLine(" 1) Log-In ");
+                Console.WriteLine(" 2) Register ");
+                Console.WriteLine(" 3) Exit" );
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("=========================");
+
+            }
+
+            
         }
     }
 }
